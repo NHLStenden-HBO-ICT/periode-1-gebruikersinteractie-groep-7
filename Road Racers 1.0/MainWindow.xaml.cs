27305234458aa53c1ca803fde181e94f803a5ebc
Rubensandy04 <rubenandyprive@gmail.com>
2023-10-20 18:22:06 +0200

    snelle naam fix

--- Road Racers 1.0/MainWindow.xaml.cs
+++ Road Racers 1.0/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
-namespace Road_Racers1._0
+namespace Road_Racers_1._0
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
